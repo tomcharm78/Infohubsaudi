cb7e91de2573952b68936b2bd9bb896fa8217fd2
tomcharm78 <123059178+tomcharm78@users.noreply.github.com>
2026-04-12 00:32:21 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: false,
+  experimental: {
+    forceSwcTransforms: false,
+  },
 };
 module.exports = nextConfig;
